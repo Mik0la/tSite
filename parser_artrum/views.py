a69e8425510a1c7e0020b0e6df2b53e21011a301
Mikola Bely <mikola.bely@gmail.com>
2017-02-27 16:28:32 +0300

    20170227 beta 1.1

--- parser_artrum/views.py
+++ parser_artrum/views.py
@@ -6,28 +6,29 @@ import os
 
 
 def index(request):
-    state = request.GET['state']
-    config = {
-        'path_parser_dir': 'parser_artrum',
-        'path_out_dir': 'out',
-        'log': 1,
-        'json_file_indent': 4,
-        'json_file': 'parser_json.txt',
-        'json_download_file': 'download_file_json.txt',
-    }
-    if state == 'start':
-        pars = ParserArtRum(**config)
-        thread = threading.Thread(target=pars.run)
-        thread.start()
-        return HttpResponse('Start parsing')
-    elif state == 'files':
-        with open(os.path.join(config['path_parser_dir'],
-                               config['path_out_dir'],
-                               config['json_download_file']), 'rt') as f:
-            return HttpResponse(f.read())
-    elif state == 'struct':
-        with open(os.path.join(config['path_parser_dir'],
-                               config['path_out_dir'],
-                               config['json_file']), 'rt') as f:
-            return HttpResponse(f.read())
+    if 'state' in request.GET:
+        state = request.GET['state']
+        config = {
+            'path_parser_dir': 'parser_artrum',
+            'path_out_dir': 'out',
+            'log': 1,
+            'json_file_indent': 4,
+            'json_file': 'parser_json.txt',
+            'json_download_file': 'download_file_json.txt',
+        }
+        if state == 'start':
+            pars = ParserArtRum(**config)
+            thread = threading.Thread(target=pars.run)
+            thread.start()
+            return HttpResponse('Start parsing')
+        elif state == 'files':
+            with open(os.path.join(config['path_parser_dir'],
+                                   config['path_out_dir'],
+                                   config['json_download_file']), 'rt') as f:
+                return HttpResponse(f.read())
+        elif state == 'struct':
+            with open(os.path.join(config['path_parser_dir'],
+                                   config['path_out_dir'],
+                                   config['json_file']), 'rt') as f:
+                return HttpResponse(f.read())
     return HttpResponse('Состояние неизвестно')
