152813daf1e22c9b6b15f1d721d6c8eccb9aae66
Mikola Bely <mikola.bely@gmail.com>
2018-05-10 18:32:26 +0300

    20180510-6

--- tSite/settings.py
+++ tSite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'parser_artrum'
+    # 'parser_artrum'
 ]
 
 MIDDLEWARE = [
